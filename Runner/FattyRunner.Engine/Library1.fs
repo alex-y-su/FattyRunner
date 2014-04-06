35e38fcd25360e4cca18371c7a5a9dcaac2bf1bf
Alexey Suvorov <x0040h@gmail.com>
2014-04-06 12:38:49 +0400

    Test run initial implementation

--- Runner/FattyRunner.Engine/Library1.fs
+++ Runner/FattyRunner.Engine/Library1.fs
@@ -1,18 +1,81 @@
 ﻿namespace FattyRunner.Engine
 
+type TestReference =
+    { Type    : System.Type
+      Run     : System.Reflection.MethodInfo
+      Init    : System.Reflection.MemberInfo option
+      Dispose : System.Reflection.MemberFilter option }
+
+type TestConfiguration = 
+    { Count  : int
+      WarmUp : int }
+
 type Test = 
-    { FullName: string }
+    { Reference     : TestReference
+      Configuration : TestConfiguration
+      Context       : Map<string,obj> option }
 
 type Configuration = 
     { Count: int 
       TestsToRun: Test list }
 
-module TestRunner =
-    
-    
+type EnvironmentConfiguration = 
+    { ProfileMemory: bool }
 
-    let WarmUp() =
+module ReflectionHelper =
+    open System
+    open System.Reflection
+
+    let instantiate (t:Type) (args: obj[])=
+        Activator.CreateInstance(t,args)
+
+module TestRunnerEngine =
+    open System
+    
+    let prepareEnvironment cfg =
         ()
 
-    type Class1() = 
-        member this.X = "F#"
+    let shutdownEnvironment cfg =
+        ()
+
+    let beforeStart config =
+        ()
+
+    let afterEnd config =
+        ()
+
+    let executeOnce x =
+        let startTime = DateTime.Now.Ticks
+        do x() |> ignore
+        let endTime = DateTime.Now.Ticks
+        endTime - startTime
+
+    let runTest (cfg: EnvironmentConfiguration) (t:Test) =
+        do prepareEnvironment cfg
+        
+        let instance = 
+            match t.Context with 
+            | Some(ctx) ->
+                ReflectionHelper.instantiate t.Reference.Type [|ctx|]
+            | None ->
+                ReflectionHelper.instantiate t.Reference.Type null
+
+        let fu() = 
+            t.Reference.Run.Invoke(instance,null) 
+
+        let decoratedExecute x = 
+            beforeStart cfg
+            let res = executeOnce x
+            afterEnd cfg
+            res
+
+        let timings = seq { 1..t.Configuration.Count } 
+                        |> Seq.map (fun x -> (x, decoratedExecute fu))
+                        |> Seq.toArray
+        
+        do shutdownEnvironment cfg
+        timings
+    
+    let run (tests:Test list) config = 
+        tests |> Seq.map (runTest config)
+    
