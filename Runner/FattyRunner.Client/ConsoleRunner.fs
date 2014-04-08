3f3f2ef5a0c7b912066192b19a5c7225e43d05ab
Alexey Suvorov <x0040h@gmail.com>
2014-04-09 00:32:04 +0400

    Test for console command arguments parser

--- Runner/FattyRunner.Client/ConsoleRunner.fs
+++ Runner/FattyRunner.Client/ConsoleRunner.fs
@@ -2,51 +2,71 @@
 
 type RunConfiguration = 
     { AssemblyLocation : string
-      TestName : string option
+      TestList : string list
       IterationsCount : uint32 option }
 
-module ConfigurationHelpers =
+module ConfigurationHelpers = 
     open Azon.Helpers.Extensions
-
-    let normalize (s:string) = s.SubstringAfter ":"
-
-    let (|Location|_|) (s: string) =
+    
+    let normalize (s : string) = s.SubstringAfter ":"
+    
+    let (|Location|_|) (s : string) = 
         if s.StartsWith("path:") then Some(normalize s)
         else None
-
-    let (|Test|_|) (s: string) =
+    
+    let (|Test|_|) (s : string) = 
         if s.StartsWith("test:") then Some(normalize s)
         else None
-
-    let (|Count|_|) (s: string) =
+    
+    let (|Count|_|) (s : string) = 
         if s.StartsWith("n:") then 
             let n = normalize s
             Some(System.UInt32.Parse n)
         else None
-
-    let rec readConfigFromArgs (args:string list) cfg = 
-        match args with
-        | h::t -> 
-            let cfg' = 
-                match h with
-                | Location(x) -> { cfg with AssemblyLocation = x }
-                | Test(x) -> { cfg with TestName = Some(x) }
-                | Count(x) -> { cfg with IterationsCount = Some(x) }
-                | _ -> cfg
-            readConfigFromArgs t cfg
-        | [] -> cfg
-
+    
     let defaultConfiguration = 
-        { AssemblyLocation = System.AppDomain.CurrentDomain.BaseDirectory
-          TestName = None
-          IterationsCount = None}
+            { AssemblyLocation = System.AppDomain.CurrentDomain.BaseDirectory
+              TestList = []
+              IterationsCount = None }
+
+
+    let readConfigFromArgs (args : string list) : RunConfiguration = 
+        let rec read' args cfg = 
+            match args with
+            | h :: t -> 
+                let cfg' = 
+                    match h with
+                    | Location(x) -> { cfg with AssemblyLocation = x }
+                    | Test(x) -> { cfg with TestList = cfg.TestList @ [ x ] }
+                    | Count(x) -> { cfg with IterationsCount = Some(x) }
+                    | _ -> cfg
+                read' t cfg'
+            | [] -> cfg
+        read' args defaultConfiguration
 
 
 module ConsoleRunner = 
     open Azon.Helpers.Extensions
     open ConfigurationHelpers
+    
+    let writeConfigurationIncorrect() =
+        printfn @"Use 
+                    n:[number]  - Count of iterations
 
-    let run args =
-        let cfg = readConfigFromArgs (args |> List.ofArray) defaultConfiguration
+                    path:[path] - File or directory where test assemblies are located.\n 
+                                  Can be used multiple times.
+                    
+                    test:[name] - Full class name + method name like ""MyNamespace.MyClass.MethodToTest"".\n
+                                  Can be used multiple times.  
+                  "
 
+    let runFile (s:string) = ()
+    let runDir (s:string) = ()
+
+    let run args = 
+        let cfg = readConfigFromArgs (args |> List.ofArray)
+        match cfg.AssemblyLocation with
+        | x when System.IO.File.Exists x -> runFile x
+        | x when System.IO.Directory.Exists x -> runDir x
+        | _ -> do writeConfigurationIncorrect()
         0
---
+++ Tests/FattyRunner.Engine.Tests/ConsoleRunnerTests.fs
@@ -0,0 +1,43 @@
+﻿namespace FattyRunner.Client.Tests
+
+module ``Console runner tests`` =
+    open Xunit
+    open FsUnit.Xunit
+    open FattyRunner.Engine
+    open FattyRunner.Client
+
+    [<Fact>]
+    let ``Should parse path to assembly``() =
+        let args = [@"path:C:\Dir\Path"]
+
+        let cfg = ConfigurationHelpers.readConfigFromArgs args
+
+        cfg.AssemblyLocation |> should equal "C:\Dir\Path"
+
+    [<Fact>]
+    let ``Should parse all test``() =
+        let args = ["test:MyClass.MyMethod1"
+                    "test:MyClass.MyMethod2"
+                    "test:MyClass.MyMethod3"]
+        
+        let cfg = ConfigurationHelpers.readConfigFromArgs args
+        
+        let arr = cfg.TestList |> List.toArray
+        cfg.TestList |> List.length |> should equal 3
+        arr.[0] |> should equal "MyClass.MyMethod1"
+        arr.[1] |> should equal "MyClass.MyMethod2"
+        arr.[2] |> should equal "MyClass.MyMethod3"
+
+    [<Fact>]
+    let ``Should parse test name``() =
+        let args = ["test:MyClass.MyMethod"]
+        let cfg = ConfigurationHelpers.readConfigFromArgs args
+        cfg.TestList |> List.length |> should equal 1
+        cfg.TestList |> List.head |> should equal "MyClass.MyMethod"
+
+    [<Fact>]
+    let ``Should parse n in command line`` () =
+        let args = ["n:1000"]
+        let cfg = ConfigurationHelpers.readConfigFromArgs args 
+        cfg.IterationsCount |> Option.get |> should equal 1000u
+
