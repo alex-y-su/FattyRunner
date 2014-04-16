79947a09fa7d8e456b61a97cbea5966bfe96d495
Alexey Suvorov <x0040h@gmail.com>
2014-04-17 02:35:53 +0400

    #7 Since f# is not good for WPF client was converted to console app

--- Runner/FattyRunner.Client/App.fs
+++ Runner/FattyRunner.Client/App.fs
@@ -1,21 +1,17 @@
-﻿module MainApp
+﻿namespace FattyRunner.Client
 
-open System
-open System.Windows
-open System.Windows.Controls
-open System.Windows.Markup
+module MainApp =
+    open System
+    open System.Windows.Controls
+    open System.Windows.Markup
 
-open FattyRunner.Client
+    open FattyRunner.Client
 
-//mainWindowViewModel.DataContext <- new MainWindowViewModel() 
+    //mainWindowViewModel.DataContext <- new MainWindowViewModel() 
 
-// Application Entry point
-[<STAThread>]
-[<EntryPoint>]
-let main args =
-    if args.Length > 0 then ConsoleRunner.run args
-    else
-        let mainWindowViewModel = 
-            Application.LoadComponent(new System.Uri("MainWindow.xaml", UriKind.Relative)) :?> Window 
-        (new Application()).Run(mainWindowViewModel)
+    // Application Entry point
+    [<STAThread>]
+    [<EntryPoint>]
+    let main args = ConsoleRunner.run args
+           
     
