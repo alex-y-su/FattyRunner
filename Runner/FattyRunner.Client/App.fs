c1dbfe3822364a2d9ab81d1e5c0f184f5e20fa4e
Alexey Suvorov <x0040h@gmail.com>
2014-04-17 02:41:37 +0400

    #7 UI project template added

--- Runner/FattyRunner.Client/App.fs
+++ Runner/FattyRunner.Client/App.fs
@@ -1,16 +1,6 @@
 ﻿namespace FattyRunner.Client
 
 module MainApp =
-    open System
-    open System.Windows.Controls
-    open System.Windows.Markup
-
-    open FattyRunner.Client
-
-    //mainWindowViewModel.DataContext <- new MainWindowViewModel() 
-
-    // Application Entry point
-    [<STAThread>]
     [<EntryPoint>]
     let main args = ConsoleRunner.run args
            
