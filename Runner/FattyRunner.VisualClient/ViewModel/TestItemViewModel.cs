3f721b6b0d1829d4fdac51e551764272e4ec604e
Alexey Suvorov <x0040h@gmail.com>
2014-05-07 21:51:47 +0400

    #7

--- Runner/FattyRunner.VisualClient/ViewModel/TestItemViewModel.cs
+++ Runner/FattyRunner.VisualClient/ViewModel/TestItemViewModel.cs
@@ -1,25 +1,41 @@
 ﻿using System.ComponentModel;
 
+using Caliburn.Micro;
+
 using FattyRunner.Engine;
 
 namespace FattyRunner.VisualClient.ViewModel {
-    public class TestItemViewModel : INotifyPropertyChanged {
+
+    public class InProgressTestResultsViewModel : TestResultsViewModel {
+        
+    }
+
+    public class TestResultsViewModel : INotifyPropertyChanged {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public class TestItemViewModel : Screen {
         public TestItemViewModel(Test test) {
             this.Test = test;
+            this.TestResults = 
         }
 
         public Test Test { get; private set; }
-        public string ClassName { get { return this.Test.Reference.Type.Name; } }
-        public string MethodName { get { return this.Test.Reference.Run.Name; } }
+
+        public TestResultsViewModel TestResults { get; private set; }
+
+        public string ClassName {
+            get { return this.Test.Reference.Type.Name; }
+        }
+
+        public string MethodName {
+            get { return this.Test.Reference.Run.Name; }
+        }
 
         public void RunTest() {
             //this._controller.RunTest(this.ActiveItem.Test, this.OnRunComplete);
         }
 
-        public void OnRunComplete(TestResult data) {
 
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
