31e0193b000886ee63c02c7be7dfecbcd8ce19cb
Alexey Suvorov <x0040h@gmail.com>
2014-05-16 22:17:54 +0400

    #9 Added visual client runner application

---
+++ Runner/FattyRunner.VisualClient.Loader/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace FattyRunner.VisualClient.Loader {
+    internal class Program {
+        [LoaderOptimization(LoaderOptimization.MultiDomainHost)]
+        [STAThread]
+        private static void Main() {
+            var startupPath = Path.GetDirectoryName(
+                Assembly
+                    .GetExecutingAssembly().Location);
+
+            var cachePath = Path.Combine(startupPath, "__cache");
+            try {
+                var configFile = Path.Combine(startupPath, "FattyRunner.VisualClient.exe.config");
+                var assembly = Path.Combine(startupPath, "FattyRunner.VisualClient.exe");
+
+                var setup = new AppDomainSetup {
+                    ApplicationName = "FattyRunnerVisualClient",
+                    ShadowCopyFiles = "true",
+                    CachePath = cachePath,
+                    ConfigurationFile = configFile
+                };
+
+                var domain = AppDomain.CreateDomain(
+                    "FattyRunnerVisualClient",
+                    AppDomain.CurrentDomain.Evidence,
+                    setup);
+
+                domain.ExecuteAssembly(assembly);
+
+                AppDomain.Unload(domain);
+            }
+            finally {
+                Directory.Delete(cachePath, true);
+            }
+        }
+    }
+}
