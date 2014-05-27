001a2c50a56f5d9a202bedbd39f5f05b96712607
Alexey Suvorov <x0040h@gmail.com>
2014-05-28 01:33:59 +0400

    Cross domain interaction now broken

--- Runner/FattyRunner.Engine/AppDomainWrapper.fs
+++ Runner/FattyRunner.Engine/AppDomainWrapper.fs
@@ -5,14 +5,21 @@ open System.IO
 open System.Reflection
 
 module AppDomainWrapper =
+    type ITestLoaderWrapper =
+        abstract member execute : EnvironmentConfiguration -> string -> TestResult list 
+
     type TestLoaderWrapper() =
-        member x.execute (cfg:EnvironmentConfiguration) (path: string) =
-            let asm = Assembly.LoadFile(path)
-            let tests = TestLoader.loadTests cfg asm |> Seq.toList
-            TestRunnerEngine.run tests cfg
+        inherit System.MarshalByRefObject()
+        interface ITestLoaderWrapper with
+            member x.execute (cfg:EnvironmentConfiguration) (path: string) =
+                let asm = Assembly.LoadFile(path)
+                let tests = TestLoader.loadTests cfg asm |> Seq.toList
+                let x = AppDomain.CurrentDomain.FriendlyName
+                TestRunnerEngine.run tests cfg
 
     let runForAssembly (cfg: EnvironmentConfiguration) (path: string) =
         let basePath = Path.GetDirectoryName path
+        let x = AppDomain.CurrentDomain.FriendlyName
 
         let runnerAsm = Assembly.GetExecutingAssembly().Location
         let runnerDir = AppDomain.CurrentDomain.BaseDirectory
@@ -20,8 +27,9 @@ module AppDomainWrapper =
         let setup = new AppDomainSetup(ApplicationName="FattyRunner", 
                                        ShadowCopyFiles = "true", 
                                        PrivateBinPath = runnerDir,
-                                       ApplicationBase = basePath)
+                                       ApplicationBase = runnerDir)
+
         let domain = AppDomain.CreateDomain(path, AppDomain.CurrentDomain.Evidence, setup)
   
-        let executor = domain.CreateInstanceFromAndUnwrap(runnerAsm, typeof<TestLoaderWrapper>.FullName) :?> TestLoaderWrapper
+        let executor = domain.CreateInstanceFromAndUnwrap(runnerAsm, typeof<TestLoaderWrapper>.FullName) :?> ITestLoaderWrapper
         executor.execute cfg path
